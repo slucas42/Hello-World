cde7f24ec284d373ca0da960323287b225a78d92
Samantha Lucas <samanthalucas@stsh-01316-samantha.lucas-mbat>
2019-11-25 14:00:04 -0500

    Homework 2

---
+++ HW2.rb
@@ -0,0 +1,47 @@
+#Below you will find my day 2 homework
+
+#if/elsif/else using puts
+
+x = 1
+
+if x == 2 
+		puts "This is true!"
+elsif x == 1 
+		puts "This is not true."
+else 
+		puts "that is not an answer available"
+end
+
+#true/false booleans
+
+if true
+		puts "this is very true!"
+end
+
+if false
+		puts "this is false!\n"
+end
+
+#array with if/else and include
+
+food_list = ["Turkey", "Mashed Potatoes", "Pie"]
+if food_list.include?("Turkey")
+		puts "This is part of dinner."
+else
+		puts "This is not on the menu."
+end
+
+#string interpolation
+
+time = "1:25PM"
+puts "The current time is #{time}."
+
+#10.times do
+
+10.times do 
+	puts "I solumnly swear that I am up to no good."
+end
+
+
+
+
